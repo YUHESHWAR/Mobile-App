ed54b02e4088cf34b9f39c5c1381e8fd1f25a6e6
YUHESHWAR <yuheshwar.ks@gmail.com>
2023-03-11 10:33:37 +0530

    Mobile app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [mobiles, setMobiles] = useState([]);
+  // const API = "http://localhost:4000";
+  const API = "https://node01-production.up.railway.app";
+  useEffect(() => {
+    fetch(`${API}/mobiles`)
+      .then((data) => data.json())
+      .then((msg) => setMobiles(msg));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App phone-list-container">
+      {mobiles.map((data) => (
+        <Phone key={data._id} data={data} />
+      ))}
+    </div>
+  );
+}
+
+function Phone({ data }) {
+  // const data = {
+  //   model: "OnePlus 9 5G",
+  //   img: "https://m.media-amazon.com/images/I/61fy+u9uqPL._SX679_.jpg",
+  //   company: "Oneplus",
+  // };
+  return (
+    <div className="phone-container">
+      <img className="phone-picture" src={data.img} alt={data.model} />
+      <h1 className="phone-name">{data.model}</h1>
+      <p className="phone-company">{data.company}</p>
     </div>
   );
 }
